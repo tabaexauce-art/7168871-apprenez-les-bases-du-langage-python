174b4b7fca7bd53378528ce95280d0253641c909
tabaexauce-art <tabaexauce@gmail.com>
2025-09-15 17:54:32 +0200

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,9 @@
-# Écrivez votre code ici !
+nom = "TABA"
+prenom = "Prince"
+age = 28
+
+print(f"je m'appelle {prenom} {nom} et j'ai {age}ans.")
+
+age = age + 10 
+
+print(f"je m'appelle {prenom} {nom} et j'ai {age}maintenant.")
